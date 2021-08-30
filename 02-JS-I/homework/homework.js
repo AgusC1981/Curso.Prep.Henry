368649e25d2f9727dd84813e4bfac74ada44d619
Agustina Cano <agustinacano8581@gmail.com>
2021-08-29 23:58:50 -0300

    modificados

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -164,9 +164,15 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero === 0) { return false;
-  } else if (Numero > 0) {return 'Es positivo';
-  } else if (numero < 0) {return 'es Negativo';
+  if(numero === 0) {
+    return false;
+  }
+  else if(numero > 0) {
+    return "Es positivo";
+  }
+  else {
+    return "Es negativo";
+  }
 }
 }
 
@@ -181,7 +187,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinar = 'nombre '+ 'apellido';
+  var combinar = nombre + ' ' +apellido;
   return combinar;
 }
 
@@ -209,7 +215,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-  return (base * Altura) / 2;
+  return (base * altura) / 2;
 }
 
 
@@ -227,7 +233,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if(letra.length > 1){
+    return "Dato incorrecto"
+  }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
 }
 
 
